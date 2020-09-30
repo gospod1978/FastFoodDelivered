4bd9d2e2f532113102398db16e98be7fbd6fa1a1
Mihail Gospodinov <54326812+gospod1978@users.noreply.github.com>
2020-09-30 13:32:59 +0200

    * AllImage.cs: functionality select Couriers
    * Startup.cs:
    * GlobalConstants.cs:
    * site.css:
    * All.cshtml:
    * Create.cshtml:
    * ApplicationDbContext.cs:
    * Create.cshtml:
    * Create.cshtml:
    * _Layout.cshtml:
    * All.cshtml:
    * AspNetCoreTemplate.Web.csproj:
    * Create.cshtml:
    * UsersController.cs:
    * RolesController.cs:
    * PostsController.cs:
    * NavBarAdmin.cshtml:
    * AspNetCoreTemplate.Common.csproj:
    * _LoginPartial.cshtml:
    * CategoriesController.cs:
    * CategoryAll.cs:
    * AllCouriersDetail.cshtml:
    * CourierAllViewModel.cs:
    * RoleServices.cs:
    * IRoleServices.cs:
    * AspNetCoreTemplate.Data.Models.csproj:
    * CourierDetailsViewModel.cs:
    * CategoriesService.cs:
    * CategoryIndexViewModel.cs:
    * UserAddRoleCreateInputModel.cs:
    * ApplicationDbContextModelSnapshot.cs:
    * CategoryCreateInputModel.cs:
    * AspNetCoreTemplate.Web.ViewModels.csproj:
    * RegisterModel.cshtml.cs:
    * AspNetCoreTemplate.Services.Data.csproj:

---
+++ src/Web/AspNetCoreTemplate.Web.ViewModels/Couriers/CourierAllViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AspNetCoreTemplate.Web.ViewModels.Couriers
+{
+    using System.Collections.Generic;
+
+    using AspNetCoreTemplate.Web.ViewModels.Cities;
+    using AspNetCoreTemplate.Web.ViewModels.Vehicle;
+
+    public class CourierAllViewModel
+    {
+        public IEnumerable<CuriersAll> Curiers { get; set; }
+
+        public IEnumerable<CitiesAll> Cities { get; set; }
+
+        public IEnumerable<VehicleAll> Vehichles { get; set; }
+    }
+}
---
+++ src/Web/AspNetCoreTemplate.Web.ViewModels/Couriers/CourierDetailsViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace AspNetCoreTemplate.Web.ViewModels.Couriers
+{
+    using System;
+
+    using AspNetCoreTemplate.Data.Models.Courier;
+    using AspNetCoreTemplate.Data.Models.FastFoodCommon;
+    using AspNetCoreTemplate.Services.Mapping;
+
+    public class CourierDetailsViewModel : IMapFrom<Courier>
+    {
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public Vehichle Vehicle { get; set; }
+
+        public City City { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
