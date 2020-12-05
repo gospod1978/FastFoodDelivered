2282381c4a521716aa6db90eb57d3aa708e8788e
Mihail Gospodinov <54326812+gospod1978@users.noreply.github.com>
2020-12-05 07:40:08 +0100

    Start Test ServiseAddress

--- src/Tests/AspNetCoreTemplate.Services.Data.Tests/AddressServiceTests.cs
+++ src/Tests/AspNetCoreTemplate.Services.Data.Tests/AddressServiceTests.cs
@@ -3,10 +3,12 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-
+    using AspNetCoreTemplate.Data;
     using AspNetCoreTemplate.Data.Common.Repositories;
     using AspNetCoreTemplate.Data.Models.Addresses;
+    using AspNetCoreTemplate.Data.Repositories;
     using AspNetCoreTemplate.Services.Data.Addresses;
+    using Microsoft.EntityFrameworkCore;
     using Moq;
     using Xunit;
 
@@ -76,8 +78,41 @@
             var service = new AddressService(mockAddress.Object, mockWArea.Object);
 
             var addressId = await service.CreateAsyncAddress("1", "1", "1", "1");
-            Assert.Equal("1", listAddress[0].CityId);
-            Assert.Equal("1", listAddress[0].AreaId);
+
+            var result = service.GetById<Address>(addressId);
+            Assert.NotNull(result);
+            Assert.Equal("1", result.AreaId);
+            Assert.Equal("1", result.CityId);
+            Assert.Equal("1", result.StreetId);
+            Assert.Equal("1", result.LocationId);
+        }
+
+        [Fact]
+        public async Task GetAddressByIdShouldReturnCorrectInfook()
+        {
+            var listAddress = new List<Address>();
+            var mockAddress = new Mock<IDeletableEntityRepository<Address>>();
+            mockAddress.Setup(x => x.All()).Returns(listAddress.AsQueryable());
+            mockAddress.Setup(x => x.AddAsync(It.IsAny<Address>())).Callback(
+                (Address address) => listAddress.Add(address));
+
+            var listWArea = new List<WorkingArea>();
+            var mockWArea = new Mock<IDeletableEntityRepository<WorkingArea>>();
+            mockWArea.Setup(x => x.All()).Returns(listWArea.AsQueryable());
+            mockWArea.Setup(x => x.AddAsync(It.IsAny<WorkingArea>())).Callback(
+                (WorkingArea workingArea) => listWArea.Add(workingArea));
+
+            var service = new AddressService(mockAddress.Object, mockWArea.Object);
+
+            var addressId = await service.CreateAsyncAddress("1", "1", "1", "1");
+
+            var result = service.GetAddress(addressId);
+
+            Assert.NotNull(result);
+            Assert.Equal("1", result.AreaId);
+            Assert.Equal("1", result.CityId);
+            Assert.Equal("1", result.StreetId);
+            Assert.Equal("1", result.LocationId);
         }
     }
 }
